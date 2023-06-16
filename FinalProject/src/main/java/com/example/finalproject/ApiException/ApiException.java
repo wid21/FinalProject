8f01fdb635842023688377eae9143749df393094
SWE - Wid Seraj <120143921+wid21@users.noreply.github.com>
2023-06-16 21:43:04 +0300

    Delete FinalProject directory

--- FinalProject/src/main/java/com/example/finalproject/ApiException/ApiException.java
+++
@@ -1,8 +0,0 @@
-package com.example.finalproject.ApiException;
-
-public class ApiException extends RuntimeException{
-
-    public ApiException(String message){
-        super(message);
-}
-    }
--- FinalProject/src/main/java/com/example/finalproject/ControllerAdvice/ControllerAdvice.java
+++
@@ -1,4 +0,0 @@
-package com.example.finalproject.ControllerAdvice;
-
-public class ControllerAdvice {
-}
